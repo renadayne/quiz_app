0e36c6a9a0cab01c35ab7e5fc6e3d5215c213dd9
renaisme <huytuduelist@gmail.com>
2022-11-27 23:50:33 +0700

    splash screen

--- app/src/main/java/com/example/quizzzzi/MainActivity.java
+++ app/src/main/java/com/example/quizzzzi/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.quizzzzi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView appName, slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        // Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        // Ánh xạ
+        logo = findViewById(R.id.logo);
+        appName = findViewById(R.id.appName);
+        slogan = findViewById(R.id.slogan);
+
+        logo.setAnimation(topAnim);
+        appName.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, QuizMenu.class);
+                startActivity(intent);
+            }
+        },5000);
     }
 }
---
+++ app/src/main/java/com/example/quizzzzi/QuizMenu.java
@@ -0,0 +1,14 @@
+package com.example.quizzzzi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_menu);
+    }
+}
