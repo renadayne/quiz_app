7366b8d6d575a5e743bc9ab833444e77ab44fa16
dohieu1852002 <dohieu825@gmail.com>
2022-11-28 15:00:06 +0700

    28/11 14h59

--- app/src/main/java/com/example/quizzzzi/QuizMenu.java
+++ app/src/main/java/com/example/quizzzzi/QuizMenu.java
@@ -1,14 +1,72 @@
 package com.example.quizzzzi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.quizzzzi.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class QuizMenu extends AppCompatActivity {
+
+//    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_quiz_menu);
+
+
+        NavHostFragment navHostFragment =(NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav,navController);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavController Navcontroller = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        switch (item.getItemId()){
+            case R.id.toGameinformation: //id item
+                //mo infor
+                Navcontroller.navigate(R.id.inforFragment);//id fragment
+                return true;
+
+            case R.id.toFeedback:
+
+                Navcontroller.navigate(R.id.feedbackFragment);
+                Intent sendmail = new Intent(Intent.ACTION_SEND);
+                sendmail.setType("text/plain");
+                sendmail.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{"Dohieu825@gmail.com"});
+                sendmail.putExtra(Intent.EXTRA_SUBJECT,"Can gop y");
+
+                if(sendmail.resolveActivity(getPackageManager()) != null){
+                    startActivity(sendmail);
+                }else{
+                    Toast.makeText(QuizMenu.this, "DOWNLOAD E-MAIL APPS", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
